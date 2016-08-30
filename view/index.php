0f7afafd90f01798d273ff36dc14bb8814856b48
alexandre <alexandre@towerc.com.br>
2016-08-29 21:27:36 -0300

    Criação da view/index.php

---
+++ view/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        
+         <title>Hospesite</title>
+         
+          <!-- Bootstrap core CSS -->
+          <link href="../webapps/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+          
+         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <a href="">Logo</a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    </body>
+</html>
+
