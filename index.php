9330759059fb610e5f56289fc560ad3fdb1a705f
alexandre <alexandre@towerc.com.br>
2016-09-23 13:23:17 -0300

    Modificação na index

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hospesite</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="webapps/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="webapps/css/hospesite.css" />
+        <script type="text/javascript" src="webapps/jquery.js"></script>
+        <script type="text/javascript" src="webapps/bootstrap/js/bootstrap.js"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a href="">Logo</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <form class="navbar-form navbar-right">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Email" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Senha" />
+                        </div>
+                        <button type="submit" class="btn btn-success">Entrar</button>
+                    </form>
+                </div>                
+            </div>            
+        </nav>
+        <div class="jumbotron">
+            <div class="container">
+                <div class="div_busca_dominio">
+                    <label class="label_busca_dominio">Registre seu Domínio</label> 
+                    <form class="form-inline">
+                        <input type="text" class="form-control" placeholder="WWW" />
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            rodape
+        </div>
+    </body>
+</html>
